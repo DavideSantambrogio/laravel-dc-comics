4edbe59c658c4cc18450023e6bd11f41465421dd
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-01-18 17:50:07 +0100

    fix: use resouce in routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\ComicController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\ComicController;
-
-Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
-Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
-Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+Route::resource('comics', ComicController::class);
