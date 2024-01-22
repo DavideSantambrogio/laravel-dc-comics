da876db18006c56247eb2199f012c1accc6f6f1c
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-01-22 16:14:07 +0100

    refactoring

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -7,7 +7,6 @@ use App\Models\Comic;
 
 class ComicController extends Controller
 {
-
     public function index()
     {
         $comics = Comic::all();
@@ -26,56 +25,56 @@ class ComicController extends Controller
     }
 
     public function store(Request $request)
-{
-    $validatedData = $request->validate([
-        'title' => 'required',
-        'description' => 'required',
-        'thumb' => 'required',
-        'price' => 'required',
-        'series' => 'required',
-        'sale_date' => 'required',
-        'type' => 'required',
-    ]);
+    {
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'thumb' => 'required',
+            'price' => 'required',
+            'series' => 'required',
+            'sale_date' => 'required',
+            'type' => 'required',
+        ]);
 
-    Comic::create($validatedData);
+        Comic::create($validatedData);
 
-    return redirect()->route('comics.index')->with('success', 'Comic creato');
-}
-
-    public function destroy($id)
-{
-    $comic = Comic::find($id);
-
-    if ($comic) {
-        $comic->delete();
-        return redirect()->route('comics.index')->with('success', 'Comic cancellato con successo');
+        return redirect()->route('comics.index')->with('success', 'Comic creato');
     }
 
-    return redirect()->route('comics.index')->with('error', 'Comic non trovato');
-}
+    public function destroy($id)
+    {
+        $comic = Comic::find($id);
+
+        if ($comic) {
+            $comic->delete();
+            return redirect()->route('comics.index')->with('success', 'Comic cancellato con successo');
+        }
+
+        return redirect()->route('comics.index')->with('error', 'Comic non trovato');
+    }
 
     public function edit($id)
-{
-    $comic = Comic::findOrFail($id);
-    return view('comics.edit', compact('comic'));
-}
-    
+    {
+        $comic = Comic::findOrFail($id);
+        return view('comics.edit', compact('comic'));
+    }
+
     public function update(Request $request, $id)
-{
-    $comic = Comic::findOrFail($id);
+    {
+        $comic = Comic::findOrFail($id);
 
-    $validatedData = $request->validate([
-        'title' => 'required',
-        'description' => 'required',
-        'thumb' => 'required',
-        'price' => 'required',
-        'series' => 'required',
-        'sale_date' => 'required',
-        'type' => 'required',
-    ]);
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'thumb' => 'required',
+            'price' => 'required',
+            'series' => 'required',
+            'sale_date' => 'required',
+            'type' => 'required',
+        ]);
 
-    $comic->update($validatedData);
+        $comic->update($validatedData);
 
-    return redirect()->route('comics.index')->with('success', 'Comic modificato con successo');
-}
+        return redirect()->route('comics.index')->with('success', 'Comic modificato con successo');
+    }
 }
